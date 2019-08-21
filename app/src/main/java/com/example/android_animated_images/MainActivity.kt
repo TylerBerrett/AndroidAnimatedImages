cf2f4a4dc9746f4296a16641e5b11e602a790055
Tyler Berrett <btylerberrett@gmail.com>
2019-08-21 14:16:49 -0600

    add play, next, back images to bottom layout add animated gif

--- app/src/main/java/com/example/android_animated_images/MainActivity.kt
+++ app/src/main/java/com/example/android_animated_images/MainActivity.kt
@@ -1,37 +1,49 @@
 package com.example.android_animated_images
 
+import android.annotation.SuppressLint
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
+import android.os.Build
 import android.os.Bundle
+import android.widget.ImageView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textMessage: TextView
+    private lateinit var imageHolder: ImageView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.button_back -> {
-                textMessage.setText(R.string.title_home)
+                //imageHolder.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.button_play -> {
-                textMessage.setText(R.string.title_dashboard)
+                //imageHolder.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.button_forward -> {
-                textMessage.setText(R.string.title_notifications)
+                //imageHolder.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
+    @SuppressLint("WrongThread")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
+        imageHolder = findViewById(R.id.image_gif_holder)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val animatedGif = ImageDecoder.decodeDrawable(ImageDecoder.createSource(resources, R.drawable.record))
+            imageHolder.setImageDrawable(animatedGif)
+            (animatedGif as AnimatedImageDrawable).start()
+        }
+
     }
 }
